b2a2419227acf9fae5eb0449c84979ba3efdecf6
keagan erasmus <keaganerasmus12@gmail.com>
2024-09-24 21:41:13 +1200

    things

---
+++ init.lua
@@ -0,0 +1,74 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.wo.relativenumber = true
+
+-- Lazy.nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then 
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin configuration
+local plugins = {
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    -- {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
+local opts = {}
+require("lazy").setup(plugins, opts)
+
+-- Telescope keybindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- local config = require("nvim-treesitter.configs")
+-- config.setup({
+--   ensure_installed = { "c","odin", "javascript", "html", "typescript" },
+--   highlight = { enable = true },
+--   indent = { enable = true },  
+-- })
+
+-- Gruvbox color scheme setup
+require("gruvbox").setup({
+    terminal_colors = true,
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true,
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
+
