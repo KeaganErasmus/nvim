82e51a9df56ab92c160a9965d9225f702cfe0c41
keagan erasmus <keaganerasmus12@gmail.com>
2025-09-23 17:49:39 +1200

    NEWWWWWWWWWWWWWWWWWWWWW

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,28 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { 'blazkowolf/gruber-darker.nvim' },
+  { 'nvim-tree/nvim-web-devicons' },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      -- 'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      -- 'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    icons = {
+      filetype = { enabled = false },
+    },
+    opts = {
+      animation = true,
+
+      -- insert_at_start = true,
+    },
+    version = '^1.0.0',
+  },
+}
